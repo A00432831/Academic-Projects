23c52fc5d935365b91303dd9c27661113e113919
A00432357 <46535033+A00432357@users.noreply.github.com>
2019-04-04 12:37:32 -0300

    With Name and Email Validation

--- BusBooking/BusBooking/App_Start/RouteConfig.cs
+++ BusBooking/BusBooking/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BusBooking
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "users", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- BusBooking/BusBooking/user.cs
+++ BusBooking/BusBooking/user.cs
@@ -11,7 +11,8 @@ namespace BusBooking
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class user
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,10 +20,17 @@ namespace BusBooking
         {
             this.transactions = new HashSet<transaction>();
         }
-    
+
+ 
         public int user_id { get; set; }
+        [Required(ErrorMessage = "Please Enter Name ")]
+        [StringLength(50, ErrorMessage = "name should be Max length of 50")]
         public string name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Email Address")]
+        [RegularExpression(".+@.+\\..+", ErrorMessage = "Please Enter Correct Email Address")]
         public string email { get; set; }
+
         public string contact { get; set; }
         public string apt_number { get; set; }
         public string street_number { get; set; }
