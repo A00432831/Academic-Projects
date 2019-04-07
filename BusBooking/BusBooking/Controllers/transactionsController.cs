f399145341b62bbcad197a9a7a5d5186af5d5835
gyaneshwar <gyaneshwar.rao.nampally@smu.ca>
2019-04-07 01:37:23 -0300

    Created 1. Buses 2. Buses_types 3. Schedules 4. Transactions 5. creditCard_type 6. Schedules 7. users
    Created 2 layouts.
    1. for user
    2. for admin.
    
    Update the versions and packages.

---
+++ BusBooking/BusBooking/Controllers/transactionsController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BusBooking;
+
+namespace BusBooking.Controllers
+{
+    public class transactionsController : Controller
+    {
+        private BUSTICKETEntities db = new BUSTICKETEntities();
+
+        // GET: transactions
+        public async Task<ActionResult> Index()
+        {
+            var transactions = db.transactions.Include(t => t.creditcard_type).Include(t => t.schedule).Include(t => t.user);
+            return View(await transactions.ToListAsync());
+        }
+
+        // GET: transactions/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            transaction transaction = await db.transactions.FindAsync(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+
+        // GET: transactions/Create
+        public ActionResult Create()
+        {
+            ViewBag.c_id = new SelectList(db.creditcard_type, "c_id", "name");
+            ViewBag.s_id = new SelectList(db.schedules, "s_id", "source");
+            ViewBag.user_id = new SelectList(db.users, "user_id", "name");
+            return View();
+        }
+
+        // POST: transactions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "t_id,nameOnCard,cardNumber,unit_price,quantity,total_price,exp_Date,createdOn,createdBy,c_id,s_id,user_id")] transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.transactions.Add(transaction);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.c_id = new SelectList(db.creditcard_type, "c_id", "name", transaction.c_id);
+            ViewBag.s_id = new SelectList(db.schedules, "s_id", "source", transaction.s_id);
+            ViewBag.user_id = new SelectList(db.users, "user_id", "name", transaction.user_id);
+            return View(transaction);
+        }
+
+        // GET: transactions/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            transaction transaction = await db.transactions.FindAsync(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.c_id = new SelectList(db.creditcard_type, "c_id", "name", transaction.c_id);
+            ViewBag.s_id = new SelectList(db.schedules, "s_id", "source", transaction.s_id);
+            ViewBag.user_id = new SelectList(db.users, "user_id", "name", transaction.user_id);
+            return View(transaction);
+        }
+
+        // POST: transactions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "t_id,nameOnCard,cardNumber,unit_price,quantity,total_price,exp_Date,createdOn,createdBy,c_id,s_id,user_id")] transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(transaction).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.c_id = new SelectList(db.creditcard_type, "c_id", "name", transaction.c_id);
+            ViewBag.s_id = new SelectList(db.schedules, "s_id", "source", transaction.s_id);
+            ViewBag.user_id = new SelectList(db.users, "user_id", "name", transaction.user_id);
+            return View(transaction);
+        }
+
+        // GET: transactions/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            transaction transaction = await db.transactions.FindAsync(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+
+        // POST: transactions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            transaction transaction = await db.transactions.FindAsync(id);
+            db.transactions.Remove(transaction);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BusBooking/BusBooking/Views/schedulesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BusBooking;
+
+namespace BusBooking.Views
+{
+    public class schedulesController : Controller
+    {
+        private BUSTICKETEntities db = new BUSTICKETEntities();
+
+        // GET: schedules
+        public async Task<ActionResult> Index()
+        {
+            var schedules = db.schedules.Include(s => s.bus);
+            return View(await schedules.ToListAsync());
+        }
+
+        // GET: schedules/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            schedule schedule = await db.schedules.FindAsync(id);
+            if (schedule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(schedule);
+        }
+
+        // GET: schedules/Create
+        public ActionResult Create()
+        {
+            ViewBag.bus_id = new SelectList(db.buses, "bus_id", "bus_name");
+            return View();
+        }
+
+        // POST: schedules/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "s_id,source,destination,date,cost,bus_id,description")] schedule schedule)
+        {
+            if (ModelState.IsValid)
+            {
+                db.schedules.Add(schedule);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.bus_id = new SelectList(db.buses, "bus_id", "bus_name", schedule.bus_id);
+            return View(schedule);
+        }
+
+        // GET: schedules/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            schedule schedule = await db.schedules.FindAsync(id);
+            if (schedule == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.bus_id = new SelectList(db.buses, "bus_id", "bus_name", schedule.bus_id);
+            return View(schedule);
+        }
+
+        // POST: schedules/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "s_id,source,destination,date,cost,bus_id,description")] schedule schedule)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(schedule).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.bus_id = new SelectList(db.buses, "bus_id", "bus_name", schedule.bus_id);
+            return View(schedule);
+        }
+
+        // GET: schedules/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            schedule schedule = await db.schedules.FindAsync(id);
+            if (schedule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(schedule);
+        }
+
+        // POST: schedules/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            schedule schedule = await db.schedules.FindAsync(id);
+            db.schedules.Remove(schedule);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
